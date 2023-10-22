6c45978b4d67f9b26eb8994854bae1e1b6855f7c
yanakhodos <yanakhodos@gmail.com>
2023-10-22 20:04:22 +0300

    upd branch mergeconflresolve

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello and world!");
+        System.out.println("");
+        System.out.println("mergeconflresolve");
+
+
     }
 }
