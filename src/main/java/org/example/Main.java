2fd1eeefd4e6ac5676e41b02e9b06dcd639c4b52
yanakhodos <yanakhodos@gmail.com>
2023-10-22 20:18:46 +0300

    upd branch master

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and world!");
-        System.out.println("");
-        System.out.println("mergeconflresolve");
-
+        System.out.println();
+        System.out.println("main");
 
     }
 }
